7d99bb3fe5db09e4caa0ee087638568641635c39
Oleg Parshin <85984441+mtmrphosis@users.noreply.github.com>
2024-01-07 12:38:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -75,6 +75,6 @@ const startTimer = () => {
 
 buttonStart.addEventListener('click', () => {
     clearInterval(interval);
-    interval = setInterval(startTimer, 10);
+    interval = setInterval(startTimer, 1000);
 });
 
